2c16040ab99a26d01058e190c5d4bce09815621e
AbreshAlex <abrehamgebeta@gmail.com>
2022-06-03 23:46:06 +0300

    print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-	char c;
+	char d;
 
-	for (c = 'a'; c <= 'z'; c++)
+	for (d = 'a'; d <= 'z'; d++)
 
-	if (c != 'e' && c != 'q')
-		putchar(c);
+	if (d != 'e' && d != 'q')
+		putchar(d);
 
 	putchar('\n');
 
