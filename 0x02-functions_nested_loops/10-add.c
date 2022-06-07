a9c583c171a68ec26e6059fa3f703bcf6d541b1f
AbreshAlex <abrehamgebeta@gmail.com>
2022-06-07 22:21:06 +0300

    10 add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - entry
+ * @x: added integer to y
+ * @y: added integer to x
+ * Return: result
+ */
+
+int add(int x, int y)
+{
+	int c;
+
+	c = x + y;
+	return (c);
+}
