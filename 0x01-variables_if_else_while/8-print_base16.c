412ef1f69e247ed79c16aae42c5f48efe416af1c
AbreshAlex <abrehamgebeta@gmail.com>
2022-06-13 01:34:05 +0300

    redo

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print the hex base
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+
+for (i = 48; i <= 63; i++)
+{
+if (i <= 57)
+putchar(i);
+else
+putchar(i + 97 - 58);
+}
+
+return (0);
+}
