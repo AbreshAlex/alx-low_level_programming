d4adf06d95b19a8fb78259326a1ffa31c2e0fda4
AbreshAlex <abrehamgebeta@gmail.com>
2022-06-12 23:54:38 +0300

    redo

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints lower case alphabets 97 to 122
+ * Return: 0
+ */
+int main(void)
+{
+char az;
+az = 'a';
+
+for (az = 'a'; az <= 'z'; az++)
+{
+putchar(az);
+}
+putchar('\n');
+
+return (0);
+}
