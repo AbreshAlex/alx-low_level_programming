805235687ca7ab7ab2eb134652f8b635d4d1f29b
AbreshAlex <abrehamgebeta@gmail.com>
2022-06-13 10:45:19 +0300

    a fun that returns the length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -10,11 +10,6 @@ int _strlen(char *s)
 int i;
 for (i = 0; s[i] != '\0'; i++)
 ;
-if (i % 10 == 0)
-_putchar('i / 10');
-else
-_putchar('i / 10');
-_putchar('i % 10');
 
 return (i);
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
 
-int _putchar(char c)
+int _putchar(char c);
 
 void reset_to_98(int *n);
 
