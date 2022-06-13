48c32b4069e6a035900a17d51ab6095ed0d9f7ff
AbreshAlex <abrehamgebeta@gmail.com>
2022-06-13 22:47:56 +0300

    added new lines

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,14 +10,19 @@ int i;
 
 for (i = 0; s[i] != '\0'; i++)
 _putchar(s[i]);
+
 _putchar('\n');
+
 for (i = 0; s[i] != '\0'; i++)
 ;
+
 i = i - 1;
+
 while (i >= 0)
 {
 _putchar(s[i]);
 i--;
 }
+
 _putchar('\n');
 }
