547332d48feca9e1764912c2ff66288047fa63e6
AbreshAlex <abrehamgebeta@gmail.com>
2022-06-20 01:32:55 +0300

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a char in a string
+ * @s: string to search
+ * @c: char to find
+ * Return: char*
+ */
+char *_strchr(char *s, char c)
+{
+int i;
+char *p;
+
+p = &c;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == *p)
+
+break;
+}
+
+
+p = &s[i];
+
+if (s[i] == '\0')
+p = 0;
+
+return (p);
+}
