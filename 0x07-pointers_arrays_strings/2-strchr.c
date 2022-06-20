51c3a19d65c753d06d4a59b63a978ea5e5c2a9d0
AbreshAlex <abrehamgebeta@gmail.com>
2022-06-20 10:23:50 -0700

    update

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,15 +8,16 @@
  */
 char *_strchr(char *s, char c)
 {
-while (*s != '\0')
+int i = 0;
+while (s[i] != '\0')
 {
-if (*s == c)
+i++;
+if (s[i] == c)
 {
-return (s);
+return (&s[i]);
 }
-s++;
 }
+return ('\0');
 
-return (0);
 }
 
