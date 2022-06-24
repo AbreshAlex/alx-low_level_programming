a4b295edab3178c70d853cb04b5d09c651f33e0d
AbreshAlex <abrehamgebeta@gmail.com>
2022-06-25 02:23:49 +0300

    redo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,18 +8,20 @@ int main(void)
 {
 int i, j;
 
-for (i = '0'; i < '8'; i++)
+for (i = '0'; i < '9'; i++)
 {
 for (j = '1'; j <= '9'; j++)
 {
 putchar(i);
 putchar(j);
+
+if (i == '8' && j == '9')
+break;
+
 putchar(',');
 putchar(' ');
 }
 }
-putchar('8');
-putchar('9');
 putchar('\n');
 
 return (0);
