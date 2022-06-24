9d38840dbd072e4a6248ccd800757de5c6b93819
AbreshAlex <abrehamgebeta@gmail.com>
2022-06-24 19:55:30 +0300

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * whatsmyname - a program that prints its name
+ * @argv: array of arguments passed
+ */
+void whatsmyname(char *argv[])
+{
+
+printf("%s\n", argv[0]);
+
+return;
+}
