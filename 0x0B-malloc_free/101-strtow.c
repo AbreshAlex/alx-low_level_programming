4e5318dfcce911b3856053502ab55c3740d52824
AbreshAlex <abrehamgebeta@gmail.com>
2022-06-26 22:53:48 +0300

    fix

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,37 +10,39 @@ char **strtow(char *str)
 	int i, j, k = 0, l = 0, c = 0;
 	char **arr;
 
+	if (str == NULL)
+	return (NULL);
+
 for (i = 0; str[i] != '\0'; i++)
 {
-	if ((str[i] != ' ' && str[i] != '\0') &&
-	(str[i + 1] == ' ' || str[i + 1] == '\0'))
+	if ((str[i] != ' ') && (str[i + 1] == ' ' || str[i + 1] == '\0'))
 		k++;
 	if (str[i] != ' ')
 		c++;
 }
-	k = k + 1;
-	c = c + k + 1;
-	arr = malloc(sizeof(char *) * k);
-	if (arr == NULL)
-	return (NULL);
-	for (i = 0; i < k; i++)
-	{
-		arr[i] = malloc(sizeof(char) * c);
-		if (arr[i] == NULL)
-		return (NULL);
-	}
+										k = k + 1;
+										c = c + k;
+arr = malloc(sizeof(char *) * k);
+if (arr == NULL)
+return (NULL);
+				for (i = 0; i < k; i++)
+				{
+					arr[i] = malloc(sizeof(char) * c);
+					if (arr[i] == NULL)
+					return (NULL);
+				}
 i = 0;
-for (j = 0; str[j] != '\0'; j++)
-{
-	if (str[j] != ' ')
-	{
-		for (l = 0; str[j] != ' ' && str[j] != '\0'; l++, j++)
-		arr[i][l] = str[j];
-		arr[i][l] = '\0';
-		i++;
-		continue;
-	}
-	continue;
-}
+			for (j = 0; str[j] != '\0'; j++)
+			{
+				if (str[j] != ' ' && str[j] != '\0')
+				{
+					for (l = 0; str[j] != ' ' && str[j] != '\0'; l++, j++)
+					arr[i][l] = str[j];
+					arr[i][l] = '\0';
+					i++;
+					continue;
+				}
+				continue;
+			}
 	return (arr);
 }
