a73622e62a47eaee2058395bc31bb10945f6ac1e
AbreshAlex <abrehamgebeta@gmail.com>
2022-07-05 00:33:23 +0300

    commit dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - new type
+ * @name: gor name
+ * @owner: dog owner
+ * @age: dog age
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif /*dog.h*/
