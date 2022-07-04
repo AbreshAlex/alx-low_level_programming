01d31b20943a1b62890f8f475b75837b95ba4cd7
AbreshAlex <abrehamgebeta@gmail.com>
2022-07-05 01:26:09 +0300

    commit 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,8 +12,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 
 	/*d = malloc(sizeof(struct dog));*/
-	/*if (d == NULL)*/
-		/*return;*/
+	if (d == NULL)
+		return;
 	d->age = age;
 	d->name = name;
 	d->owner = owner;
